af0fea1b275da664ba52edf93f506b802325553c
Yan Sun <tsun812@gmail.com>
2021-11-29 21:25:41 -0800

    modified assertArraysEqual, assertEqual, eqObjects

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,16 +2,13 @@ const assertArrayEqual = function(arr1, arr2) {
   const checkmark = String.fromCodePoint(0x2705)
   const nomark = String.fromCodePoint(0x274C);
   if (arr1.length !== arr2.length) {
-    console.log(`${nomark} ${arr1} and ${arr2} are not the same.`);
-    return;
+    return `${nomark} ${arr1} and ${arr2} are not the same.`;
   }
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      console.log(`${nomark} ${arr1} and ${arr2} are not the same.`);
-      return;
+      return `${nomark} ${arr1} and ${arr2} are not the same.`;
     }
   }
-  console.log(`${checkmark} ${arr1} and ${arr2} are the same.`);
-  return;
+  return `${checkmark} ${arr1} and ${arr2} are the same.`;
 };
 module.exports = assertArrayEqual;
--- assertEqual.js
+++ assertEqual.js
@@ -1,16 +1,12 @@
 const assertEqual = function(actual, expected) {
-  const checkmark = String.fromCodePoint(0x2705)
-  const nomark = String.fromCodePoint(0x274C)
+  // const checkmark = String.fromCodePoint(0x2705)
+  // const nomark = String.fromCodePoint(0x274C)
   if (actual === expected) {
-    console.log(`${checkmark} Assertion Passed: ${actual} === ${expected}`);
+    //console.log(`${checkmark} Assertion Passed: ${actual} === ${expected}`);
+    return true
   } else {
-    console.log(nomark + "Assertion Failed: " + actual + " !== " + expected);
+    //console.log(nomark + "Assertion Failed: " + actual + " !== " + expected);
+    return false;
   }
 };
-
-// TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual("Lighthouse Labs", "Lighthouse Labs");
-// assertEqual(1, 1);
-// assertEqual(1, 2);
 module.exports = assertEqual;
